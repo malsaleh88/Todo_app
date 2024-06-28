7da7feb5c85a0043b03e00643ff7f6fb4a8bbf94
malsaleh <m.elsaleh88@gmail.com>
2024-06-28 12:24:36 +0200

    create collection

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ client = MongoClient(mongo_uri)
 # Connect to the 'todo_app' database
 db = client.todo_app
 
+
+tasks_collection = db.tasks
+
 @app.route('/')
 def hello_world():
     return 'Hello World! Connected to MongoDB!'
